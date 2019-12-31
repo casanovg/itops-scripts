81a234ce1e798c8549292da9b8a0a36b59b5074f
casanovg <gustavo.casanova@gmail.com>
2019-12-31 15:33:11 -0300

    Update scripts Tue 31 Dec 2019 03:33:11 PM -03

--- utils/git-sync.sh
+++ utils/git-sync.sh
@@ -17,5 +17,5 @@ git add -A .
 if [ ! -z $(git commit -m "Update scripts $(date)" | grep -w "$TXT_NO_CHANGES") ]; then
     git push
 else
-    echo "Local branch is up to date with origin ..."
+    echo "Local branch is up to date with origin ... "
 fi

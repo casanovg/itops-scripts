906209c60541c7db003564e04fc38df8b28bccd2
casanovg <gustavo.casanova@gmail.com>
2019-12-31 15:49:55 -0300

    Update scripts Tue 31 Dec 2019 03:49:55 PM -03

--- utils/git-sync.sh
+++ utils/git-sync.sh
@@ -2,7 +2,9 @@
 
 # Synchronize git repository
 # ...........................................
-# 2019-12-31 gcasanova@hellermanntyton.com.ar
+# 2019-12-31 gcasanova@hellermanntyton.com.arFOLDER_1="bare-metal"
+FOLDER_2="fileserver-vm"
+FOLDER_3="utils"
 
 GIT_REP="itops-scripts"
 TXT_NO_CHANGES="nothing to commit"
@@ -13,7 +15,11 @@ cd ~/"$GIT_REP"
 git pull
 
 # Push latest local changes
-git add -A .
+for DIR in $(ls -d -1 */); do
+    echo "Adding changes from $DIR ..."
+    git add -A .
+done
+
 if [ ! -z $(git commit -m "Update scripts $(date)" | grep -w "$TXT_NO_CHANGES") ]; then
     git commit -m "Update scripts $(date)"
     git push

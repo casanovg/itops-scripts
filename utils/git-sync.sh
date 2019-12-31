d35e90ed2c620ab7a622b8bd6b74223de50c0113
casanovg <gustavo.casanova@gmail.com>
2019-12-31 16:10:42 -0300

    Update scripts Tue 31 Dec 2019 04:10:42 PM -03

--- utils/git-sync.sh
+++ utils/git-sync.sh
@@ -3,10 +3,9 @@
 # Synchronize git repository
 # ...........................................
 # 2019-12-31 gcasanova@hellermanntyton.com.arFOLDER_1="bare-metal"
-FOLDER_2="fileserver-vm"
-FOLDER_3="utils"
 
 GIT_REP="itops-scripts"
+TXT_COMMIT="Update scripts"
 TXT_NO_CHANGES="nothing to commit"
 
 cd ~/"$GIT_REP"
@@ -17,17 +16,14 @@ cd ~/"$GIT_REP"
 # Push latest local changes
 for DIR in $(ls -d -1 */); do
     cd ~/"$GIT_REP"/$DIR
-    pwd
-    sleep 2
-    echo "Adding changes from $DIR ..."
+    sleep 1
+    echo "Adding changes from $(pwd) ..."
     git add -A .
 done
 
 #git commit -m "Update scripts $(date)"
 
-if [ ! "$(git commit -m "Update scripts $(date)" | grep -w "$TXT_NO_CHANGES")" ]; then
-    #git commit -m "Update scripts $(date)"
-    #git push
+if [ ! "$(git commit -m "$TXT_COMMIT $(date)" | grep -w "$TXT_NO_CHANGES")" ]; then
     echo "Changes commited, pushing to origin ..."
     git push
 else

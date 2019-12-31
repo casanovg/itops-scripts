aaf970c618208bc27c5502d1f2182265964fc424
casanovg <gustavo.casanova@gmail.com>
2019-12-31 16:51:24 -0300

    Update scripts Tue 31 Dec 2019 04:51:24 PM -03

--- bare-metal/stop-and-reboot.sh
+++ bare-metal/stop-and-reboot.sh
@@ -4,24 +4,23 @@
 # ...........................................
 # 2019-12-29 gcasanova@hellermanntyton.com.ar
 
-ESSENTIAL_NET_SERVICE="`cat ~/EssentialNetServices`";
+ESSENTIAL_NET_SERVICE="$(cat ~/EssentialNetServices)"
 
 # Stop all virtual machines
-echo "";
-echo "Stopping virtual machines ...";
-~/itops-scripts/bare-metal/vboxall-off.sh;
+echo ""
+echo "Stopping virtual machines ..."
+~/itops-scripts/bare-metal/vboxall-off.sh
 # Stopping essential net services
-echo "Stopping $ESSENTIAL_NET_SERVICE essential network services, Bye Bye!";
-echo "";
-~/itops-scripts/bare-metal/vbox-off.sh $ESSENTIAL_NET_SERVICE;
-sleep 3;
+echo "Stopping $ESSENTIAL_NET_SERVICE essential network services, Bye Bye!"
+echo ""
+~/itops-scripts/bare-metal/vbox-off.sh $ESSENTIAL_NET_SERVICE
+sleep 3
 # Reboot
-echo "";
-echo "Rebooting ...";
-sudo /usr/sbin/reboot;
+echo ""
+echo "Rebooting ..."
+sudo /usr/sbin/reboot
 # Start all virtual machines (in case that reboot fails)
-sleep 180;
-echo "";
-echo "Starting virtual machines ...";
-~/itops-scripts/bare-metal/vboxall-on.sh;
-
+sleep 180
+echo ""
+echo "Starting virtual machines ..."
+~/itops-scripts/bare-metal/vboxall-on.sh

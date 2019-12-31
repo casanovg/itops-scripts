9e971ea42c80951695b0a0c246ab6a6605def942
casanovg <gustavo.casanova@gmail.com>
2019-12-31 00:29:41 -0300

    Update bkp-mount.sh, bkp-umount.sh, and 2 more files...

---
+++ fileserver-vm/bkp-umount.sh
@@ -0,0 +1,21 @@
+#
+# Unount network shares for backup
+# ================================ 
+# 2018-05-16 Gustavo Casanova
+#
+echo ""
+echo "Disconnecting network shares to /mnt-backintime directory ..."
+echo ""
+sleep 1
+echo "Unmounting HTA-BusinessMate ..."
+umount /mnt-backintime/hta-businessmate
+echo "."
+sleep 1
+echo "Unmounting HTA-Mothership ..."
+umount /mnt-backintime/hta-mothership
+echo "."
+sleep 1
+echo "Unmounting HTA-Opportunity ..."
+umount /mnt-backintime/hta-opportunity
+echo "."
+sleep 1

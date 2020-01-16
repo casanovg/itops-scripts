bd8b93b6f27dfbcd1210c4a8f933cacca01d8641
casanovg <gustavo.casanova@gmail.com>
2020-01-16 19:01:36 -0300

    Update scripts Thu 16 Jan 2020 07:01:36 PM -03

--- bare-metal/install-virtualbox.sh
+++ bare-metal/install-virtualbox.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 
-# Install VirtualBox
+# Install VirtualBox on bare-metal server
 # ...........................................
 # 2020-01-16 gcasanova@hellermanntyton.com.ar
 
 NEW_VBOX_LINK="https://download.virtualbox.org/virtualbox/6.1.2/VirtualBox-6.1-6.1.2_135662_fedora31-1.x86_64.rpm"
 NEW_VBOX="VirtualBox-6.1"
 OLD_VBOX="VirtualBox-6.0"
+DELAY=10
 
-~/itops-scripts/bare-metal/vboxall-off.sh
-sudo wget $NEW_VBOX_LINK 
+# ~/itops-scripts/bare-metal/vboxall-off.sh
+echo "Getting VirtualBox repo ..."
+sudo wget $NEW_VBOX_LINK
+echo "Importing VirtualBox repo ..."
 sudo sudo rpm --import oracle_vbox.asc
+echo "Stopping all VirtualBox processes ..."
+sudo kill $(ps -C virtualbox -o pid | grep -v PID)
+sudo kill $(ps -C VirtualBox -o pid | grep -v PID)
+sudo kill $(ps -C VBox -o pid | grep -v PID)
+echo "Waiting "$DELAY" seconds ..."
+sleep $DELAY
+echo "Removing old VirtualBox version ..."
 sudo dnf remove -y $OLD_VBOX
+echo "Installing new VirtualBox version ..."
 sudo dnf install -y $NEW_VBOX
-sudo rm -f *.rpm
-~/itops-scripts/bare-metal/vboxall-on.sh
-
+echo "Removing installation files ..."
+sudo rm -f VirtualBox*.rpm
+# ~/itops-scripts/bare-metal/vboxall-on.sh

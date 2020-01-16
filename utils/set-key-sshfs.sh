4af7b85401b897b5da5640a2832718d08e1053ca
casanovg <gustavo.casanova@gmail.com>
2020-01-16 11:57:00 -0300

    Update scripts Thu 16 Jan 2020 11:57:00 AM -03

---
+++ utils/set-key-sshfs.sh
@@ -0,0 +1,12 @@
+#
+# Key for sshfs on
+# - HTA-BusinessMate (10.6.17.30)
+# - HTA-Enterprise (10.6.17.50)
+# - HTA-NAS (10.6.17.70)
+# - Passphrase: No passphrase 
+#
+#ssh-keygen -t rsa
+#ssh-copy-id -i ~/.ssh/id_rsa.pub root@10.6.17.30
+ssh-copy-id -i ~/.ssh/id_rsa.pub root@10.6.17.40
+ssh-copy-id -i ~/.ssh/id_rsa.pub root@10.6.17.50
+#ssh-copy-id -i ~/.ssh/id_rsa.pub root@10.6.17.70

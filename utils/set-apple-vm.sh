cee3f4e965bccb1c5ceb28c9f67294cc22114ae6
casanovg <gustavo.casanova@gmail.com>
2020-01-20 15:35:12 -0300

    Update scripts Mon 20 Jan 2020 03:35:12 PM -03

--- utils/set-apple-vm.sh
+++ utils/set-apple-vm.sh
@@ -4,7 +4,7 @@
 # ...........................................
 # 2020-01-18 gcasanova@hellermanntyton.com.ar
 
-APPLE_VM="$APPLE_VM"
+APPLE_VM="HTA-Apple"
 
 VBoxManage modifyvm "$APPLE_VM" --cpuidset 00000001 000106e5 00100800 0098e3fd bfebfbff
 VBoxManage setextradata "$APPLE_VM" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "iMac11,3"
@@ -12,3 +12,6 @@ VBoxManage setextradata "$APPLE_VM" "VBoxInternal/Devices/efi/0/Config/DmiSystem
 VBoxManage setextradata "$APPLE_VM" "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Iloveapple"
 VBoxManage setextradata "$APPLE_VM" "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
 VBoxManage setextradata "$APPLE_VM" "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
+
+# VBoxManage setextradata "$APPLE_VM" VBoxInternal2/EfiGraphicsResolution 1920x1080
+

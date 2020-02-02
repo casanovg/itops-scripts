b1c016fb60ddb2f865c31ffb8988ce0e12a5c139
netbackup <netbackup@hta-cloud.htargentina.lan>
2020-02-02 18:00:07 -0300

    Update scripts Sun 02 Feb 2020 06:00:07 PM -03

---
+++ virtual-machines/cloud/vm-setup/setup-postfix-gmail.sh
@@ -0,0 +1,13 @@
+#
+#
+#
+
+MY_EMAIL_ADDRESS=itops.hta@gmail.com
+MY_EMAIL_PASSWORD=tgfoyvzndituckta
+MY_SMTP_SERVER=smtp.gmail.com
+MY_SMTP_SERVER_PORT=587
+sudo echo "[$MY_SMTP_SERVER]:$MY_SMTP_SERVER_PORT $MY_EMAIL_ADDRESS:$MY_EMAIL_PASSWORD" >> /etc/postfix/password_maps
+sudo chmod 600 /etc/postfix/password_maps
+sudo unset MY_EMAIL_PASSWORD
+sudo history -c
+

e19c5b5498e2ad6d05636797c51cda78463b50cc
netbackup <netbackup@hta-cloud.htargentina.lan>
2020-02-05 00:00:30 -0300

    Update scripts Wed 05 Feb 2020 12:00:30 AM -03

--- virtual-machines/cloud/backup-seafile-db.sh
+++ virtual-machines/cloud/backup-seafile-db.sh
@@ -16,21 +16,21 @@ rm -rf /data/seafile-data/database-backup/ccnet_db*
 # Backup updated ccnet_db
 echo ""
 echo "Backing ccnet_db up ..."
-mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt ccnet_db > /data/seafile-data/database-backup/ccnet_db.sql.$(date +"%Y-%m-%d-%H-%M-%S")
+mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt ccnet_db > /data/seafile-data/database-backup/ccnet_db.sql.$(date +"%Y-%m-%d_%H-%M-%S")
 
 # Delete previous seafile_db
 rm -rf /data/seafile-data/database-backup/seafile_db*
 # Backup updated seafile_db
 echo ""
 echo "Backing seafile_db up ..."
-mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt seafile_db > /data/seafile-data/database-backup/seafile_db.sql.$(date +"%Y-%m-%d-%H-%M-%S")
+mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt seafile_db > /data/seafile-data/database-backup/seafile_db.sql.$(date +"%Y-%m-%d_%H-%M-%S")
 
 # Delete previous seahub_db
 rm -rf /data/seafile-data/database-backup/seahub_db*
 # Backup updated seahub_db
 echo ""
 echo "Backing seahub_db up ..."
-mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt seahub_db > /data/seafile-data/database-backup/seahub_db.sql.$(date +"%Y-%m-%d-%H-%M-%S")
+mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt seahub_db > /data/seafile-data/database-backup/seahub_db.sql.$(date +"%Y-%m-%d_%H-%M-%S")
 
 
 # Start Seafile services

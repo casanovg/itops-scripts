527d7c9c37b49152415637f108fea409628c2716
netbackup <netbackup@hta-cloud.htargentina.lan>
2020-02-04 21:00:07 -0300

    Update scripts Tue 04 Feb 2020 09:00:07 PM -03

---
+++ virtual-machines/cloud/backup-seafile-db.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#
+# Backup Seafile MySQL (MariaDB) databases
+# ...............................................................
+# 2020-02-02 gcasanova@hellermanntyton.com.ar
+#
+
+# Stop Seafile services
+echo ""
+echo "Stopping Seafile services ..."
+sudo systemctl stop nginx seahub seafile
+
+# Delete previous ccnet_db
+rm -rf /data/seafile-data/database-backup/ccnet_db*
+# Backup updated ccnet_db
+echo ""
+echo "Backing ccnet_db up ..."
+mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt ccnet_db > /data/seafile-data/database-backup/ccnet_db.sql.$(date +"%Y-%m-%d-%H-%M-%S")
+
+# Delete previous seafile_db
+rm -rf /data/seafile-data/database-backup/seafile_db*
+# Backup updated seafile_db
+echo ""
+echo "Backing seafile_db up ..."
+mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt seafile_db > /data/seafile-data/database-backup/seafile_db.sql.$(date +"%Y-%m-%d-%H-%M-%S")
+
+# Delete previous seahub_db
+rm -rf /data/seafile-data/database-backup/seahub_db*
+# Backup updated seahub_db
+echo ""
+echo "Backing seahub_db up ..."
+mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt seahub_db > /data/seafile-data/database-backup/seahub_db.sql.$(date +"%Y-%m-%d-%H-%M-%S")
+
+
+# Start Seafile services
+echo ""
+echo "Starting Seafile services ..."
+sudo systemctl start seafile seahub nginx
+echo ""

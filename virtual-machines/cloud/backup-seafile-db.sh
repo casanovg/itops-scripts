c526222e413c68d453879b2e52bb26db1cd60a80
netbackup <netbackup@hta-cloud.htargentina.lan>
2020-02-10 14:00:06 -0300

    Update scripts Mon 10 Feb 2020 02:00:06 PM -03

--- virtual-machines/cloud/backup-seafile-db.sh
+++ virtual-machines/cloud/backup-seafile-db.sh
@@ -6,6 +6,9 @@
 # 2020-02-02 gcasanova@hellermanntyton.com.ar
 #
 
+BACKUP-SYSTEM="/data/seafile-data/backup-system"
+BACKUP-DATABASE="/data/seafile-data/backup-database"
+
 # Stop Seafile services
 echo ""
 echo "Stopping Seafile services ..."
@@ -17,25 +20,25 @@ SERVICE_STATUS="$(systemctl is-active mariadb)"
 if [ "$SERVICE_STATUS" = "active" ]; then
 
     # Delete previous ccnet_db
-    rm -rf /data/seafile-data/database-backup/ccnet_db*
+    rm -rf "$BACKUP-DATABASE"/ccnet_db*
     # Backup updated ccnet_db
     echo ""
     echo "Backing ccnet_db up ..."
-    mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt ccnet_db > /data/seafile-data/database-backup/ccnet_db.sql.$(date +"%Y-%m-%d_%H-%M-%S")
+    mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt ccnet_db > "$BACKUP-DATABASE/ccnet_db.sql.$(date +"%Y-%m-%d_%H-%M-%S")"
 
     # Delete previous seafile_db
-    rm -rf /data/seafile-data/database-backup/seafile_db*
+    rm -rf "$BACKUP-DATABASE"/seafile_db*
     # Backup updated seafile_db
     echo ""
     echo "Backing seafile_db up ..."
-    mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt seafile_db > /data/seafile-data/database-backup/seafile_db.sql.$(date +"%Y-%m-%d_%H-%M-%S")
+    mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt seafile_db > "$BACKUP-DATABASE/seafile_db.sql.$(date +"%Y-%m-%d_%H-%M-%S")"
 
     # Delete previous seahub_db
-    rm -rf /data/seafile-data/database-backup/seahub_db*
+    rm -rf "$BACKUP-DATABASE"/seahub_db*
     # Backup updated seahub_db
     echo ""
     echo "Backing seahub_db up ..."
-    mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt seahub_db > /data/seafile-data/database-backup/seahub_db.sql.$(date +"%Y-%m-%d_%H-%M-%S")
+    mysqldump -h localhost -uroot -p"$(~/itops-scripts/virtual-machines/cloud/vm-setup/get-mysql-root-pwd.sh)" --opt seahub_db > "$BACKUP-DATABASE/seahub_db.sql.$(date +"%Y-%m-%d_%H-%M-%S")"
 
 else
     echo ""

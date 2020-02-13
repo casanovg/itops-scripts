e58bc133f1638523bf0921a54cdbedafddf0f63f
casanovg <gustavo.casanova@gmail.com>
2020-02-13 15:00:07 -0300

    Update scripts Thu 13 Feb 2020 03:00:07 PM -03

---
+++ bare-metal/vm-restart.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+# Script to restart a single virtual machine
+# ............................................
+# 2020-02-13 gcasanova@hellermanntyton.com.ar
+
+VM=$(vboxmanage list vms | gawk -F\" '{print $(NF-1)}' | grep -w "^$1$")
+WAIT_TIME=300 # If the virtual machine does not stop in 5 minutes, exit
+EXIT_CODE=0
+
+if [ $# -eq 0 ] || [ -z "$1" ]
+then
+    echo ""
+    echo "Usage: vm-restart <VIRTUAL-MACHINE>"
+    echo ""
+else
+
+	if [ ! -z "$VM" ]; then
+		RUNNING_VM=$(vboxmanage list runningvms | gawk -F\" '{print $(NF-1)}' | grep -w "^$VM$")
+		# Stop virtual machine
+		if [ ! -z "$RUNNING_VM" ]; then
+			echo ""
+			echo ""$VM" virtual machine active, sending shutdown signal ..."
+			vboxmanage controlvm "$VM" acpipowerbutton
+			sleep 2
+			vboxmanage controlvm "$VM" acpipowerbutton
+			while [[ "$RUNNING_VM" && $WAIT_TIME -gt 0 ]]; do
+				RUNNING_VM=$(vboxmanage list runningvms | gawk -F\" '{print $(NF-1)}' | grep -w "^$VM$")
+				((WAIT_TIME = WAIT_TIME - 1))
+				sleep 1
+			done
+			echo ""
+		else
+			echo ""
+			echo ""$VM" virtual machine not active ..."
+			echo ""
+			#EXIT_CODE=1
+		fi
+		echo "$VM virtual machine stopped!"
+		echo ""
+		echo -n "."
+		sleep 1
+		echo -n "."
+		sleep 1
+		echo -n "."
+		sleep 1
+		# Start virtual machine
+		echo ""
+		echo Starting "$VM virtual machine ..."
+		echo ""
+		vboxmanage startvm "$VM" --type headless
+	else
+		echo ""
+		echo ""$1" virtual machine not found!"
+		echo ""
+		EXIT_CODE=2
+	fi
+fi
+echo ""
+exit $EXIT_CODE

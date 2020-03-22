a902d7b0358917c95e071bb152889f0976e63e6b
netbackup <netbackup@nb-hibou.htargentina.lan>
2020-03-22 18:03:12 -0300

    Update scripts Sun 22 Mar 2020 06:03:12 PM -03

--- virtual-machines/cloud/start-nb-cloud.sh
+++ virtual-machines/cloud/start-nb-cloud.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 #
-# Start NB cloud file sync services
-# ...............................................................
-# 2020-02-17 gcasanova@hellermanntyton.com.ar
+# Start Hibou Seafile cloud file sync services
+# .............................................
+# 2020-03-22 gcasanova@hellermanntyton.com.ar
 #
 
 TARGET_CLOUD="iqn.2020-02.lan.htargentina:hta-freenas:hibou"
-DEV_CLOUD="sdb1"
+DEV_CLOUD_UUID="790a7298-3169-44c6-b568-7e37d4d80b55" # Run "sudo blkid" to get this 
 SERVER_CLOUD="10.6.17.70"
 
 SHARE_WARNING="SHARE_NOT_MOUNTED"
@@ -15,33 +15,52 @@ ISCSI_WARNING="DISK_NOT_MOUNTED"
 
 CL_DIR="/data/seafile-data"
 
-# Connect HTA cloud iscsi target
-if [ ! "$(cat /proc/partitions | grep -w "$DEV_CLOUD")" ]; then
+# Start Hibou cloud services
+echo ""
+echo "Discovering available iSCSI targets disks on $SERVER_CLOUD"
+echo "......................................................."
+
+sudo iscsiadm -m discovery -t sendtargets -p $SERVER_CLOUD
+
+echo ""
+echo "Connecting iSCSI disks and mounting directories"
+echo "..............................................."
+
+# Connect Hibou cloud iscsi target
+if [ -z "$(sudo blkid | grep "$DEV_CLOUD_UUID")" ]; then
+    echo "Connecting \"$TARGET_CLOUD\" iSCSI disk ..." 
     sudo iscsiadm -m node --targetname $TARGET_CLOUD -p $SERVER_CLOUD --login
-    sleep 1
+    sleep 2
+else
+    echo "\"$TARGET_CLOUD\" iSCSI disk already connected ..." 
 fi
-# Mount HTA cloud Seafile storage
-if [ "$(cat /proc/partitions | grep -w "$DEV_CLOUD")" ]; then
+
+# Mount Hibou cloud Seafile storage
+if [ ! -z "$(sudo blkid | grep "$DEV_CLOUD_UUID")" ]; then
     if [ ! -z "$(ls -1 $CL_DIR | grep $ISCSI_WARNING)" ]; then
-        sudo mount /dev/$DEV_CLOUD $CL_DIR
+	    echo "Mounting \"$CL_DIR\" directory ..."
+        sudo mount /dev/disk/by-uuid/$DEV_CLOUD_UUID $CL_DIR
+    else
+	    echo "\"$CL_DIR\" directory already mounted ..."
     fi
+else
+    echo "Unable to mount \"$CL_DIR\", target iSCSI disconected!"
 fi
 
 sleep 5
 
 # Start cloud services
+echo ""
+echo "Initializing Hibou Seafile cloud sync service"
+echo "............................................."
 for SERVICE in seafile seahub nginx; do
-    if [ $(systemctl is-active ${SERVICE}) == "inactive" ]; then
+    if [ $(sudo systemctl is-active ${SERVICE}) != "active" ]; then
         echo "Starting ${SERVICE} ..."
         sudo systemctl start ${SERVICE}
     else
         echo "Service ${SERVICE} already running ..."
     fi
+    sleep 1
 done
+echo ""
 
-# Services restart
-# for ACTION in stop start ; do
-#     for SERVICE in seafile seahub nginx; do
-#       systemctl ${ACTION} ${SERVICE}
-#     done
-# done

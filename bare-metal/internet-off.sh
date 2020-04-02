72c7bf2eabb222843e0ea2f90cb981c01a5c73c2
casanovg <gustavo.casanova@gmail.com>
2020-04-02 17:00:07 -0300

    Update scripts Thu 02 Apr 2020 05:00:07 PM -03

--- bare-metal/internet-off.sh
+++ bare-metal/internet-off.sh
@@ -12,16 +12,18 @@ EXIT_CODE=0
 
 for INTERNET in "HTA-NetPal" "HTA-Firewall 2"; do
 
-	if [ ! -z "$INTERNET$" ]; then
-		RUNNING_VM=$(vboxmanage list runningvms | gawk -F\" '{print $(NF-1)}' | grep -w "^$INTERNET$")
+	VM=$(vboxmanage list vms | gawk -F\" '{print $(NF-1)}' | grep -w "^$INTERNET$")
+	echo --- $VM
+	if [ ! -z "$VM" ]; then
+		RUNNING_VM=$(vboxmanage list runningvms | gawk -F\" '{print $(NF-1)}' | grep -w "$VM")
 		if [ ! -z "$RUNNING_VM" ]; then
 			echo ""
-			echo ""$INTERNET" virtual machine active, sending shutdown signal ..."
-			vboxmanage controlvm "$INTERNET" acpipowerbutton
+			echo ""$RUNNING_VM" virtual machine active, sending shutdown signal ..."
+			vboxmanage controlvm "$RUNNING_VM" acpipowerbutton
 			sleep 2
-			vboxmanage controlvm "$INTERNET" acpipowerbutton
+			vboxmanage controlvm "$RUNNING_VM" acpipowerbutton
 			while [[ "$RUNNING_VM" && $WAIT_TIME -gt 0 ]]; do
-				RUNNING_VM=$(vboxmanage list runningvms | gawk -F\" '{print $(NF-1)}' | grep -w "^$INTERNET$")
+				RUNNING_VM=$(vboxmanage list runningvms | gawk -F\" '{print $(NF-1)}' | grep -w "^$VM$")
 				((WAIT_TIME = WAIT_TIME - 1))
 				#echo "$WAIT_TIME"
 				sleep 1
@@ -29,15 +31,15 @@ for INTERNET in "HTA-NetPal" "HTA-Firewall 2"; do
 			echo ""
 		else
 			echo ""
-			echo ""$INTERNET" virtual machine not active!"
+			echo ""$RUNNING_VM" virtual machine not active!"
 			EXIT_CODE=1
 		fi
 	else
-		#echo ""
-		echo ""$INTERNET" virtual machine not found!"
 		echo ""
+		echo "\"$VM\" virtual machine not found!"
 		EXIT_CODE=2
 	fi
+	sleep 1
 
 done
 

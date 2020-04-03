b43bfa08e43185e6d3929ce7585eba7aef0011cf
casanovg <gustavo.casanova@gmail.com>
2020-04-03 00:00:07 -0300

    Update scripts Fri 03 Apr 2020 12:00:07 AM -03

--- bare-metal/net-monitor.sh
+++ bare-metal/net-monitor.sh
@@ -1,10 +1,10 @@
  
 declare -i TARGETS=0
 #declare -i TESTED=0
-declare -i SECONDS=0
-declare -i MINUTES=0
+declare -i SEC=0
+declare -i MIN=0
 
-INTERNET_TARGET_1="10.6.17.36"
+INTERNET_TARGET_1="10.6.17.37"
 LOCAL_TARGET_1="10.6.17.1"
 LOCAL_TARGET_2="10.6.17.41"
 
@@ -18,15 +18,26 @@ echo ""
 
 
 # Check internet
-while ! ping -c1 -w2 "$INTERNET_TARGET_1" &> /dev/null; do
 #while ! ping -c1 -w2 "$INTERNET_TARGET_1" &> /dev/null; do
-  echo "down" $MINUTES:$SECONDS
-  sleep 10
-  SECONDS=SECONDS+10
-  if test $SECONDS -ge 60; then
-    SECONDS=0
-    MINUTES=MINUTES+1
+while [ "$(ping -c1 -w2 "$INTERNET_TARGET_1")" ]; do
+  echo " Total downtime: " $MIN:$SEC
+
+  if [ $MIN == 3 ] && [ $SEC == 30 ]; then
+    echo ""
+    echo "   WOW!"
+    echo ""
+    break
   fi
+
+  sleep 10
+  
+  SEC=SEC+10
+  
+  if [ $SEC == 60 ]; then
+    SEC=0
+    MIN=MIN+1
+  fi
+
 done
 
 echo "Sarasa starting ..."

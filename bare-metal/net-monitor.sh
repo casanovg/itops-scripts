8bc4d6c5be1ee033fcc50547ead53f779b277bb0
casanovg <gustavo.casanova@gmail.com>
2020-04-03 02:00:07 -0300

    Update scripts Fri 03 Apr 2020 02:00:07 AM -03

--- bare-metal/net-monitor.sh
+++ bare-metal/net-monitor.sh
@@ -1,6 +1,5 @@
- 
 declare -i TARGETS=0
-#declare -i TESTED=0
+declare -i TESTED=0
 declare -i SEC=0
 declare -i MIN=0
 
@@ -8,31 +7,33 @@ INTERNET_TARGET_1="10.6.17.36"
 LOCAL_TARGET_1="10.6.17.1"
 LOCAL_TARGET_2="10.6.17.41"
 
-for TGT in $INTERNET_TARGET_1 $LOCAL_TARGET_1 $LOCAL_TARGET_2; do
-	TARGETS=TARGETS+1
-	echo " Target $TARGETS -->" $TGT
+echo ""
+for TGT in $INTERNET_TARGET_1 2 3 "10.6.17.37" 5; do
+  TARGETS=TARGETS+1
+  echo "Target $TARGETS -->" $TGT
 done
 
+echo
 echo "Targets to test: $TARGETS"
 echo ""
 
-
 # Check internet
-while ! ping -c1 -w2 "$INTERNET_TARGET_1" &> /dev/null; do
+while ! ping -c1 -w2 "$INTERNET_TARGET_1" &>/dev/null; do
 
-  echo " Total downtime: " $MIN:$SEC
+  echo "Total downtime: " $MIN:$SEC
 
-  if [ $MIN == 3 ] && [ $SEC == 30 ]; then
+  if [ $MIN == 0 ] && [ $SEC == 10 ]; then
     echo ""
-    echo "   WOW!"
+    echo "  WOW! Target failed ..."
     echo ""
+    TESTED=2**TARGETS
     break
   fi
 
   sleep 10
-  
+
   SEC=SEC+10
-  
+
   if [ $SEC == 60 ]; then
     SEC=0
     MIN=MIN+1
@@ -40,7 +41,9 @@ while ! ping -c1 -w2 "$INTERNET_TARGET_1" &> /dev/null; do
 
 done
 
-echo "Sarasa starting ..."
+echo " Sarasa starting: $TESTED ..."
 echo ""
 
+declare -i JOT=TESTED & 16
 
+echo "Lalucha: $JOT"

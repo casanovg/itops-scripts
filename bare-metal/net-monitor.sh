50cea19c5a166a6bea58337920db25c6d0f6beee
casanovg <gustavo.casanova@gmail.com>
2020-04-02 22:00:07 -0300

    Update scripts Thu 02 Apr 2020 10:00:07 PM -03

--- bare-metal/net-monitor.sh
+++ bare-metal/net-monitor.sh
@@ -1,14 +1,32 @@
+ 
+declare -i TARGETS=0
 
+INTERNET_TARGET_1="8.8.8.8"
+LOCAL_TARGET_1="10.6.17.37"
+
+for TARGET in $INTERNET_TARGET_1 $LOCAL_TARGET_1; do
+	TARGETS=TARGETS+1;
+done
+
+echo "Targets to test: " $TARGETS
+echo ""
 
 declare -i s=0
 declare -i m=0
-while ! ping -c1 -w2 8.8.8.8 &> /dev/null ;
-do
+
+# Check internet
+while [ -z "$(ping -c1 -w2 "$INTERNET_TARGET_1" &> /dev/null)" ]; do
+#while ! ping -c1 -w2 "$INTERNET_TARGET_1"; do
   echo "down" $m:$s
   sleep 10
   s=s+10
-  if test $s -ge 60; then
+  if [ $s -ge 60 ]; then
     s=0
     m=m+1;
   fi
 done
+
+echo "Sarasa starting ..."
+echo ""
+
+

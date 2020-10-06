c1f499523e242f7d14551839216446ff47c652f0
casanovg <gustavo.casanova@gmail.com>
2020-10-05 23:00:57 -0300

    Update scripts Mon 05 Oct 2020 11:00:57 PM -03

--- utils/serial-monitor.sh
+++ utils/serial-monitor.sh
@@ -9,6 +9,15 @@ SERIAL_PORT=/dev/ttyUSB0
 
 clear
 
+if [ -f $SERIAL_PORT ]; then
+	echo "Opening port";
+else
+	echo
+	echo "Port $SERIAL_PORT not found";
+	echo
+	exit 1;
+fi
+
 echo
 echo "Serial monitor starting at $SERIAL_BPS on port $SERIAL_PORT ..."
 echo

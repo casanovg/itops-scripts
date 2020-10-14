e98cbcaa4ba59862e20a6e548402e3c4a702c42e
Gustavo Casanova <gustavo.casanova@gmail.com>
2020-10-14 17:20:06 -0300

    Update scripts Wed 14 Oct 2020 05:20:05 PM -03

--- utils/git-update-id.sh
+++ utils/git-update-id.sh
@@ -1,15 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 # Update GitHub user and password ***
 # ...........................................
 # 2020-01-03 gcasanova@hellermanntyton.com.ar
 
+clear
+
 echo ""
 echo "·····································"
 echo "· Please enter your GitHub username ·"
 echo "·····································"
-read usr
-echo "$usr" | openssl aes-256-cbc -pbkdf2 -pass pass:' ' > ~/.github-usr
+echo -n " > "
+read USR
+echo "$USR" | openssl aes-256-cbc -pbkdf2 -pass pass:' ' > ~/.github-usr
 
 echo ""
 echo "·····································"
@@ -17,37 +20,59 @@ echo "· Please enter your GitHub password ·"
 echo "·····································"
 stty_orig=$(stty -g)
 stty -echo
-read pwd 1>/dev/null
+echo -n " > "
+read PWD 1>/dev/null
 stty $stty_orig
-echo "$pwd" | openssl aes-256-cbc -pbkdf2 -pass pass:' ' > ~/.github-pwd
+echo "$PWD" | openssl aes-256-cbc -pbkdf2 -pass pass:' ' > ~/.github-pwd
 echo ""
 
 stty sane
 
+GIT_USER_NAME="$(git config --list | grep 'user.name')"
+GIT_USER_EMAIL="$(git config --list | grep 'user.email')"
 
-if [ -z "$(git config --list | grep 'user.name')" ]; then
+if [ -z "$GIT_USER_NAME" ]; then
         echo "·······························"
         echo "· Please enter your full name ·"
         echo "·······························"
-        read fullname
-        git config --global user.name "$fullname"
+        read FULLNAME
+        git config --global user.name "$FULLNAME"
 fi
 
-if [ -z "$(git config --list | grep 'user.email')" ]; then
+if [ -z "$GIT_USER_EMAIL" ]; then
         echo "····························"
         echo "· Please enter your e-mail ·"
         echo "····························"
-        read email
-        git config --global user.email "$email"
+        read EMAIL
+        git config --global user.email "$EMAIL"
 fi
 
-echo "#############################################################################"
+clear
+
+echo ""
+echo "#######################################################################"
 echo "# ATTENTION!"
-echo "# Your GitHub user name is: \"$usr\" and your new password: \"$pwd\""
-echo "#############################################################################"
+echo -n "# GitHub username set to: [ $USR ] password: [ "
+
+LPWD=${#PWD}
+SHOW=$(( LPWD * 1 / 3 ))
+
+for (( i=0; i<$LPWD; i++ )); do
+        if [ $i -lt $((LPWD-$SHOW)) ]; then
+                echo -n "*";
+        else
+                echo -n "${PWD:$i:1}";
+        fi
+done;
+echo " ]"
+
+echo "#######################################################################"
+echo "# Git user : $(echo "${GIT_USER_NAME#*"="}")"
+echo "# Git eMail: $(echo "${GIT_USER_EMAIL#*"="}")"
+echo "#######################################################################"
 echo ""
 
 stty sane
-sleep 2
+sleep 3
 clear
 

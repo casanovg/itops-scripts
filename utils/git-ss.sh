c2f962afe9193cec950f91ecff679848af983dda
Gustavo Casanova <gustavo.casanova@gmail.com>
2020-10-14 11:09:48 -0300

    Update scripts Wed 14 Oct 2020 11:09:43 AM -03

--- utils/git-ss.sh
+++ utils/git-ss.sh
@@ -17,7 +17,7 @@ if [ ! $(git rev-parse --is-inside-work-tree 2>>/dev/null) ]; then
     echo "You are NOT inside a git repository! Please change the current"
     echo "directory to a git folder or specify its path as follows:"
     echo
-    echo "git-sync.sh /my-repos/my-git-project-folder"
+    echo "git-sync.sh ~/my-repos/my-git-project-folder"
     echo
     exit 1
 fi

599444e70e92098c5c6c70937a85998e82c22106
Gustavo Casanova <gustavo.casanova@gmail.com>
2020-10-14 14:19:16 -0300

    Update scripts Wed 14 Oct 2020 02:19:16 PM -03

--- utils/git-update-id.sh
+++ utils/git-update-id.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Update GitHub user and password
+# Update GitHub user and password ***
 # ...........................................
 # 2020-01-03 gcasanova@hellermanntyton.com.ar
 
@@ -22,41 +22,24 @@ stty $stty_orig
 echo "$pwd" | openssl aes-256-cbc -pbkdf2 -pass pass:' ' > ~/.github-pwd
 echo ""
 
-if [ -z $(git config --list | grep 'user.name') ]; then
-	echo "·······························"
-	echo "· Please enter your full name ·"
-	echo "·······························"
-	read fullname
-	git config --global user.name "$fullname"
+stty sane
+
+
+if [ -z "$(git config --list | grep 'user.name')" ]; then
+        echo "·······························"
+        echo "· Please enter your full name ·"
+        echo "·······························"
+        read fullname
+        git config --global user.name "$fullname"
 fi
 
-if [ -z $(git config --list | grep 'user.email') ]; then
-	echo "····························"
-	echo "· Please enter your e-mail ·"
-	echo "····························"
-	read email
-	git config --global user.email "$email"
+if [ -z "$(git config --list | grep 'user.email')" ]; then
+        echo "····························"
+        echo "· Please enter your e-mail ·"
+        echo "····························"
+        read email
+        git config --global user.email "$email"
 fi
-
-
-SGUS="Gustavo Adrian Casanova"
-LGUS=${#SGUS}
-SHOW=$(( LGUS * 1 / 3 ))
-
-echo $SHOW
-
-
-for (( i=0; i<${#SGUS}; i++ )); do
-        if [ $i -lt $((LGUS-$SHOW)) ]; then
-                echo -n "*"
-        else
-                echo -n "${SGUS:$i:1}";
-        fi
-done;
-echo
-
-
-
 
 echo "#############################################################################"
 echo "# ATTENTION!"

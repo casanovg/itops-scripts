4d3a700a4041bf7a032ee1a7590d86c262de1d2d
casanovg <gustavo.casanova@gmail.com>
2021-01-15 03:40:10 -0300

    Update scripts Fri 15 Jan 2021 03:40:10 AM -03

--- virtual-machines/fileserver/fs-start.sh
+++ virtual-machines/fileserver/fs-start.sh
@@ -10,8 +10,7 @@ echo""
 echo "Joining HTARGENTINA Domain"
 echo "--------------------------"
 echo "Using \"fs-usr\" domain administrator password ..."
-#sudo net ads join -U administrator%$pwd -S hta-pdc.htargentina.lan -I 10.6.17.45
-sudo net ads join -U administrator%$pwd -S hta-pdn.htargentina.lan -I 10.6.17.47
+sudo net ads join -U administrator%$pwd -S hta-pdc.htargentina.lan -I 10.6.17.45
 sudo winbindd
 sudo smbd
 sudo nmbd

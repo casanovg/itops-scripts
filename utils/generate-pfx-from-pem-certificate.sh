097bbbb2d075382a4bd1e2028589fd71e107f8da
netbackup <netbackup@hta-cloud.htargentina.lan>
2021-01-24 15:28:17 -0300

    Update scripts Sun 24 Jan 2021 03:28:17 PM -03

---
+++ utils/generate-pfx-from-pem-certificate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# Generate .pfx from Let's Encrypt .pem certificate and key for IIS
+# =================================================================
+# 2021-01-24 gcasanova@hellermanntyton.com.ar
+#
+
+LETSENCRYPT="/etc/letsencrypt/archive"
+CERTIFICATE="$1"
+
+openssl pkcs12 -export -in $LETSENCRYPT/$CERTIFICATE/fullchain1.pem -inkey $LETSENCRYPT/$CERTIFICATE/privkey1.pem -out $CERTIFICATE.pfx
+

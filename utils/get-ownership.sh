6b66eb44c6865142a7d3f559552d889787530d81
netbackup <netbackup@hta-cloud.htargentina.lan>
2021-02-03 17:31:11 -0300

    Update scripts Wed 03 Feb 2021 05:31:11 PM -03

--- utils/get-ownership.sh
+++ utils/get-ownership.sh
@@ -8,16 +8,16 @@
 
 
 OPT_1="--with-permissions"
-OPT_2="--show-errors"
+OPT_2="--no-errors"
 
-if [ $# -eq 0 ] || [ -z "$1" ] || [ "$1" == "$OPT_1" ]
+if [ $# -eq 0 ] || [ -z "$1" ] || [ "$1" == "$OPT_1" ] || [ "$1" == "$OPT_2" ]
 then
     echo ""
-    echo "Usage: get-ownership.sh <PATH> [$OPT_1]"
+    echo "Usage: get-ownership.sh <PATH> [$OPT_1] [$OPT_2]"
     echo ""
 else
 
-    if [ $# -ge 2 ] && [ "$2" != "$OPT_1" ]
+    if [ $# -ge 2 ] && [ "$2" != "$OPT_1" ] && [ "$2" != "$OPT_2" ]
     then
 	echo
         echo " $2: option not found"
@@ -25,10 +25,10 @@ else
 	exit 1
     fi  
 
-    if [ $# -ge 3 ] && [ "$3" != "$OPT_2" ]
+    if [ $# -ge 3 ] && [ [ "$3" != "$OPT_1" ] || [ "$3" != "$OPT_2" ] ]
     then
 	echo
-        echo " $2: option not found"
+        echo " $3: option not found"
 	echo
 	exit 1
     fi  
@@ -37,17 +37,31 @@ else
     then
         echo ""
         for path in $(find $1); do
-            echo -n "if [ -d $path ] || [ -f $path ]; then sudo chown $(stat -c %U:%G $path) $path; "
-            if [ "$2" == "$OPT_1" ]
+
+	    if [ "$2" == "$OPT_2" ] || [ "$3" == "$OPT_2" ]
 	    then
-                echo "sudo chmod $(stat -c %a $path) $path;"
+                echo -n "if [ -d $path ] || [ -f $path ]; then "
+            fi
+
+            echo -n "sudo chown $(stat -c %U:%G $path) $path; "
+
+            if [ "$2" == "$OPT_1" ] || [ "$3" == "$OPT_1" ]
+	    then
+                echo -n "sudo chmod $(stat -c %a $path) $path; "
             fi  
-            echo "fi"    
+
+	    if [ "$2" == "$OPT_2" ] || [ "$3" == "$OPT_2" ]
+	    then
+                echo -n "fi"
+	    fi
+
+	    echo ""
+
         done
 	echo ""
     else
         echo ""
-        echo "$1: path not found"
+        echo " $1: path not found"
         echo ""
     fi
 

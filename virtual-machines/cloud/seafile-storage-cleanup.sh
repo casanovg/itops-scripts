390a51108d60333b5f4cda3fac5069385b0e9828
netbackup <netbackup@hta-cloud.htargentina.lan>
2021-02-05 20:35:07 -0300

    Update scripts Fri 05 Feb 2021 08:35:07 PM -03

--- virtual-machines/cloud/seafile-storage-cleanup.sh
+++ virtual-machines/cloud/seafile-storage-cleanup.sh
@@ -20,47 +20,65 @@
 
 SEAFILE_PATH="/opt/seafile"
 SERVICE_USER="netbackup"
+LOG_PATH="/data/seafile-data/maintenance-logs"
+LOG_FILE="Seafile-Maintenance-$(date +%Y-%m-%d)"
+
+if [ ! -d $LOG_PATH ]; then
+    mkdir -p $LOG_PATH
+fi    
+
+rm -rf $LOG_PATH/* 1>/dev/null 2>/dev/null
+
+echo 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+echo "Seafile Maintenance Routine" 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+echo "===========================" 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+date 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+echo 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
 
 # Stop the server
-echo Stopping the Seafile server ...
-sudo systemctl stop seafile.service
-sudo systemctl stop seahub.service
-echo
+echo Stopping the Seafile server ... 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+sudo systemctl stop seafile.service 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+sudo systemctl stop seahub.service 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+echo 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
 
-echo Giving the server some time to shut down properly ...
+echo Giving the server some time to shut down properly ... 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
 sleep 20
-echo
+echo 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
 if [ 1 == 2 ]; then
 # Check and repair libraries
-echo Seafile FSCK started ...
-sudo -u $SERVICE_USER $SEAFILE_PATH/seafile-server-latest/seaf-fsck.sh --repair
-echo
+#echo Seafile FSCK started ...
+sudo -u $SERVICE_USER $SEAFILE_PATH/seafile-server-latest/seaf-fsck.sh --repair 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+echo 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+
+sleep 10
 
 # Run the garbage collection
-echo Seafile cleanup started ...
-sudo -u $SERVICE_USER $SEAFILE_PATH/seafile-server-latest/seaf-gc.sh
-echo
+echo Seafile cleanup started ... 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+sudo -u $SERVICE_USER $SEAFILE_PATH/seafile-server-latest/seaf-gc.sh 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+echo 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
 fi
-# Clear expired session records
-echo Clear expired session records ...
-sudo -u $SERVICE_USER $SEAFILE_PATH/seafile-server-latest/seahub.sh python-env $SEAFILE_PATH/seafile-server-latest/seahub/manage.py clearsessions
-echo
 
-# Clear outdated library records
-echo Clear outdated library records ...
-sudo -u $SERVICE_USER $SEAFILE_PATH/seafile-server-latest/seahub.sh python-env $SEAFILE_PATH/seafile-server-latest/seahub/manage.py clear_invalid_repo_data
-echo
-
-echo Giving the server some time ...
 sleep 10
-echo
+
+# Clear expired session records
+echo Clear expired session records ... 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+sudo -u $SERVICE_USER $SEAFILE_PATH/seafile-server-latest/seahub.sh python-env $SEAFILE_PATH/seafile-server-latest/seahub/manage.py clearsessions 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+echo 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+
+sleep 10
 
 # Start the server again
-echo Starting the Seafile server ...
-sudo systemctl start seafile.service
-sudo systemctl start seahub.service
-echo
+echo Starting the Seafile server ... 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+sudo systemctl start seafile.service 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+sudo systemctl start seahub.service 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+echo 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
 
-echo Seafile cleanup done!
-echo
+# Clear outdated library records
+echo Clear outdated library records ... 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+sudo -u $SERVICE_USER $SEAFILE_PATH/seafile-server-latest/seahub.sh python-env $SEAFILE_PATH/seafile-server-latest/seahub/manage.py clear_invalid_repo_data 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+echo 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+date 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+echo 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+echo Seafile cleanup done! 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
+echo 1>>$LOG_PATH/$LOG_FILE 2>>$LOG_PATH/$LOG_FILE
 

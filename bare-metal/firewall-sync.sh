6d8c7417e66aa53a2253407f950069a7456c498a
casanovg <gustavo.casanova@gmail.com>
2021-03-23 23:57:55 -0300

    Update scripts Tue 23 Mar 2021 11:57:55 PM -03

--- bare-metal/firewall-sync.sh
+++ bare-metal/firewall-sync.sh
@@ -4,13 +4,11 @@
 # ..............................................................
 # 2020-03-22 gcasanova@hellermanntyton.com.ar
 
-FIREWALL_VM="HTA-NetPal"
-#FIREWALL_VM="HTA-Firewall"
+FIREWALL_VM="HTA-Firewall"
 PING_TIMEOUT=2
 BARE_METAL_1=10.6.17.30
 BARE_METAL_2=10.6.17.40
 BARE_METAL_3=10.6.17.50
-#BARE_METALS_ACTIVE=0
 THIS_BARE_METAL=$(sudo hostname -s | tr a-z A-Z)
 THIS_BARE_METAL_IP=$(sudo ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '192.')
 
@@ -27,7 +25,6 @@ if [ "$(vboxmanage list runningvms | gawk -F\" '{print $(NF-1)}' | grep -w "^$FI
 	    else
 	        echo -n "$BARE_METAL "
 	        if ping -c 1 -w $PING_TIMEOUT $BARE_METAL &>/dev/null; then
-	            #BARE_METALS_ACTIVE=$((BARE_METALS_ACTIVE + 1))
 	            echo "active, syncing ..."
 		    ~/itops-scripts/bare-metal/vm-move-out.sh $FIREWALL_VM $BARE_METAL --disk-only
 	        else
@@ -46,3 +43,4 @@ else
     echo ""
 
 fi
+

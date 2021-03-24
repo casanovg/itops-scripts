a30c3bcab33e68b4e20e4328a9f9dc2fe6af1bbf
netbackup <netbackup@hta-cloud.htargentina.lan>
2021-03-24 16:35:08 -0300

    Update scripts Wed 24 Mar 2021 04:35:08 PM -03

--- utils/generate-pfx-from-pem-certificate.sh
+++ utils/generate-pfx-from-pem-certificate.sh
@@ -7,6 +7,19 @@
 #
 
 LETSENCRYPT="/etc/letsencrypt/archive"
+
+if [ $# -eq 0 ] || [ -z "$1" ]
+  then
+    echo ""
+    echo "Usage: generate-pfx-from-pem-certificate.sh <CERTIFICATE_NAME>"
+    echo ""
+    echo "Certificates existing on this machine:"
+    echo ""
+    sudo ls -1 $LETSENCRYPT
+    echo ""
+    exit 1
+fi
+
 CERTIFICATE="$1"
 
 sudo openssl pkcs12 -export -in $LETSENCRYPT/$CERTIFICATE/fullchain1.pem -inkey $LETSENCRYPT/$CERTIFICATE/privkey1.pem -out $CERTIFICATE.pfx

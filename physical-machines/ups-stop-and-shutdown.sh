d03aeafa1e7be601b09bcd02fe617920cd526776
Gustavo Casanova <gustavo.casanova@gmail.com>
2023-02-05 22:05:07 -0300

    Update scripts Sun Feb  5 10:05:07 PM -03 2023

---
+++ physical-machines/ups-stop-and-shutdown.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Stop all virtual machines and shut down this server
+# in response to a command from the APC UPS via Powerchute
+# .............................................................
+# NOTE: This script is to be run under the 'root' user context
+# .............................................................
+# 2023-02-05 gustavo.casanova@gmail.com
+
+IFS=$'\n'
+
+for VM in $(sudo -H -u netbackup bash -c "vboxmanage list runningvms" | gawk -F\" '{print $(NF-1)}') ; do
+	
+	WAIT_TIME=180	# Time to wait for machine shutdown in seconds
+
+	echo "Shutting $VM down ...";
+		
+	sudo -H -u netbackup bash -c "vboxmanage controlvm $VM acpipowerbutton"
+	sleep 2
+	sudo -H -u netbackup bash -c "vboxmanage controlvm $VM acpipowerbutton"
+
+	RUNNING_VM=$VM
+	while [[ "$RUNNING_VM" && $WAIT_TIME -gt 0 ]]; do
+		RUNNING_VM="$(sudo -H -u netbackup bash -c "vboxmanage list runningvms"| gawk -F\" '{print $(NF-1)}' | grep -w "^$VM$" )"
+		if [ "$RUNNING_VM" ]; then
+			echo "$RUNNING_VM still on, waiting $WAIT_TIME seconds ..."
+		fi
+            	((WAIT_TIME = WAIT_TIME - 1))
+            	sleep 1
+		if [ $WAIT_TIME == 0 ]; then
+			echo
+			echo ">>> Timeout: $WAIT_TIME for $VM, moving on ... <<<"
+		fi
+       	done
+       	echo ""
+
+done
+
+# Shutdown
+echo "Shutting down this server ..."
+#sudo shutdown now
+

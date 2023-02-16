4439f4b0aaee8de8692593fad203114e034663cc
Gustavo Casanova <gustavo.casanova@gmail.com>
2023-02-16 12:40:08 -0300

    Update scripts Thu Feb 16 12:40:08 PM -03 2023

--- virtual-machines/fileserver/fs-start.sh
+++ virtual-machines/fileserver/fs-start.sh
@@ -4,7 +4,8 @@
 # .......................................
 # 2019-12-09 gustavo.casanova@gmail.com
 
-pwd=$(sudo cat ~/.fs-pwd | openssl aes-256-cbc -d -pbkdf2 -pass pass:' ')
+ADM_USR="$(cat ~/.fs-usr | openssl aes-256-cbc -d -pbkdf2 -pass pass:' ')"
+ADM_PWD="$(cat ~/.fs-pwd | openssl aes-256-cbc -d -pbkdf2 -pass pass:' ')"
 
 DC_FQDN="hta-adp.hellermanntyton.ar"
 DC_ADDR="10.6.17.41"
@@ -16,7 +17,7 @@ echo""
 echo "Joining HellermannTyton.ar Domain"
 echo "---------------------------------"
 echo "Using \"fs-usr\" domain administrator password ..."
-sudo net ads join -U administrator%"$pwd" -S $DC_FQDN -I $DC_ADDR
+sudo net ads join -U "$ADM_USR"%"$ADM_PWD" -S $DC_FQDN -I $DC_ADDR
 sudo winbindd
 sudo smbd
 sudo nmbd
@@ -26,3 +27,4 @@ sudo getent group $USR_GRP
 # sudo net rpc rights grant $ADM_GRP SeDiskOperatorPrivilege -U $ADM_USR%$pwd -I $DC_ADDR
 # sudo net rpc rights list privileges SeDiskOperatorPrivilege -U $ADM_USR%$pwd -I $DC_ADDR
 echo ""
+

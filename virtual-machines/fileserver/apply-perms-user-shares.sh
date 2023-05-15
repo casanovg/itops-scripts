57cb62d4c58a903da5f00ad8f4e53a6cca9c6e6f
Gustavo Casanova <gustavo.casanova@gmail.com>
2023-05-15 15:40:08 -0300

    Update scripts Mon May 15 03:40:08 PM -03 2023

--- virtual-machines/fileserver/apply-perms-user-shares.sh
+++ virtual-machines/fileserver/apply-perms-user-shares.sh
@@ -4,11 +4,11 @@
 # ......................................i...........
 # 2023-05-15 gustavo.casanova@gmail.com
 
-USR_FLD_PATH="ls -1 /data/netfiles-disk/hta-users"
+USR_FLD_PTH="/data/netfiles-disk/hta-users"
 
-for USER in $(ls -1); do
+for USER in $(ls -1 $USR_FLD_PTH); do
        echo "Applying permissions to $USER personal folder"
-       sudo chown -R "HTAR\$USER":wheel $USR_FLD_PATH/"$USER"/
+       sudo chown -R "HTAR\\$USER":wheel $USR_FLD_PTH/"$USER"/
        sleep 1
 done
 

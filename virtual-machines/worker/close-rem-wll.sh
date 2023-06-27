561fcc8f632bb25272ba50e5931ab4547b065ea3
Gustavo Casanova <gustavo.casanova@gmail.com>
2023-06-27 19:04:05 -0300

    Update scripts Tue Jun 27 07:04:05 PM -03 2023

---
+++ virtual-machines/worker/close-rem-wll.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Script to synchronize and close a remote machine's wllt
+# ........................................................
+# 2023-06-27 gustavo.casanova@gmail.com
+
+REMOTE_USR="netbackup"
+WLL_MACHINE="10.77.77.77"
+WLL_PR_NAME="raptoreum-qt"
+
+RPID=$(ssh $REMOTE_USR@$WLL_MACHINE "pgrep $WLL_PR_NAME")
+ssh $REMOTE_USR@$WLL_MACHINE "kill $RPID"
+sleep 3
+ssh $REMOTE_USR@$WLL_MACHINE "sudo shutdown -r now"
+

f3addef059773bf4c9b4c2d671c0bf4591b4d226
Gustavo Casanova <gustavo.casanova@gmail.com>
2023-09-24 06:35:21 -0300

    Update scripts Sun Sep 24 06:35:21 AM -03 2023

--- physical-machines/ups-stop-and-shutdown.sh
+++ physical-machines/ups-stop-and-shutdown.sh
@@ -14,33 +14,43 @@ ComputerLog="/home/netbackup/ups-control.log"
 echo "........................................................................................................................" >> $ComputerLog;
 echo ">>> $(hostname -s | tr [a-z] [A-Z]) server shutdown sequence initiated by UPS on $(date) ..." >> $ComputerLog;
 
-for VM in $(sudo -H -u netbackup bash -c "vboxmanage list runningvms" | gawk -F\" '{print $(NF-1)}') ; do
+
+if command -v vboxmanage &> /dev/null; then
+        if [ ! -n "$(vboxmanage --version | grep "WARNING")" ]; then
+        
+		echo "Apagando las maquinas virtuales"
+
+
+		for VM in $(sudo -H -u netbackup bash -c "vboxmanage list runningvms" | gawk -F\" '{print $(NF-1)}') ; do
 	
-	WAIT_TIME=180	# Time to wait for machine shutdown in seconds
+			WAIT_TIME=180	# Time to wait for machine shutdown in seconds
 
-	echo "Shutting $VM down ...";
-	echo "Shutting $VM down on $(date "+%b %d, %Y - %T")..." >> $ComputerLog;
+			echo "Shutting $VM down ...";
+			echo "Shutting $VM down on $(date "+%b %d, %Y - %T")..." >> $ComputerLog;
 		
-	sudo -H -u netbackup bash -c "vboxmanage controlvm $VM acpipowerbutton"
-	sleep 2
-	sudo -H -u netbackup bash -c "vboxmanage controlvm $VM acpipowerbutton"
+			sudo -H -u netbackup bash -c "vboxmanage controlvm $VM acpipowerbutton"
+			sleep 2
+			sudo -H -u netbackup bash -c "vboxmanage controlvm $VM acpipowerbutton"
 
-	RUNNING_VM=$VM
-	while [[ "$RUNNING_VM" && $WAIT_TIME -gt 0 ]]; do
-		RUNNING_VM="$(sudo -H -u netbackup bash -c "vboxmanage list runningvms"| gawk -F\" '{print $(NF-1)}' | grep -w "^$VM$" )"
-		if [ "$RUNNING_VM" ]; then
-			echo "$RUNNING_VM still on, waiting $WAIT_TIME seconds ..."
-		fi
-            	((WAIT_TIME = WAIT_TIME - 1))
-            	sleep 1
-		if [ $WAIT_TIME == 0 ]; then
-			echo
-			echo ">>> Timeout: $WAIT_TIME for $VM, moving on ... <<<"
-		fi
-       	done
-       	echo ""
+			RUNNING_VM=$VM
+			while [[ "$RUNNING_VM" && $WAIT_TIME -gt 0 ]]; do
+				RUNNING_VM="$(sudo -H -u netbackup bash -c "vboxmanage list runningvms"| gawk -F\" '{print $(NF-1)}' | grep -w "^$VM$" )"
+				if [ "$RUNNING_VM" ]; then
+					echo "$RUNNING_VM still on, waiting $WAIT_TIME seconds ..."
+				fi
+	        	    	((WAIT_TIME = WAIT_TIME - 1))
+		            	sleep 1
+				if [ $WAIT_TIME == 0 ]; then
+					echo
+					echo ">>> Timeout: $WAIT_TIME for $VM, moving on ... <<<"
+				fi
+		       	done
+		       	echo ""
 
-done
+	done
+
+        fi
+fi
 
 # Shut down
 echo "Shutting down this server ..."

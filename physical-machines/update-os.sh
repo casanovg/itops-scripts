9ef9997d6db2197bd76925f9be17293e7714c42a
Gustavo Casanova <gustavo.casanova@gmail.com>
2023-10-07 09:08:53 -0300

    Update scripts Sat Oct  7 09:08:53 AM -03 2023

--- physical-machines/update-os.sh
+++ physical-machines/update-os.sh
@@ -19,15 +19,26 @@ echo ""
 echo "Looking for updates ..."
 sudo dnf -y update
 sleep 3
+
 # Stopping essential net services
 echo ""
 echo "Stopping $ESSENTIAL_NET_SERVICE essential network services, Bye!"
-~/itops-scripts/physical-machines/vm-off.sh "$ESSENTIAL_NET_SERVICE"
-sleep 3
+if command -v vboxmanage &> /dev/null; then
+	if [ ! -n "$(vboxmanage --version | grep "WARNING")" ]; then
+		~/itops-scripts/physical-machines/vm-off.sh "$ESSENTIAL_NET_SERVICE"
+		sleep 3
+	fi
+fi
+
 # Reboot
 echo ""
 echo "Rebooting ..."
 sudo shutdown -r now
+
 # Start all virtual machines (in case that reboot fails)
-~/itops-scripts/physical-machines/vboxall-on.sh
+if command -v vboxmanage &> /dev/null; then
+	if [ ! -n "$(vboxmanage --version | grep "WARNING")" ]; then
+		~/itops-scripts/physical-machines/vboxall-on.sh
+	fi
+fi
 

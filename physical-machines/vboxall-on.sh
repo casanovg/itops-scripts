3d149a1f3c1c543be0f92fb9f26ccb06dbc8d0e4
Gustavo Casanova <gustavo.casanova@gmail.com>
2025-12-17 15:33:36 -0300

    Update scripts Wed Dec 17 03:33:36 PM -03 2025

--- physical-machines/vboxall-on.sh
+++ physical-machines/vboxall-on.sh
@@ -4,18 +4,26 @@
 # .................................................
 # 2019-12-29 gustavo.casanova@gmail.com
 
-#!/bin/sh
-
 POST_START_DLY=120
 IFS=$'\n'
 
 for VM in $(cat ~/ActiveVMs); do
-	~/itops-scripts/physical-machines/vm-on.sh "$VM"
-	if [ $? -eq 0 ]; then
-		echo "Waiting $POST_START_DLY seconds for VM's services startup ..."
-		sleep $POST_START_DLY
-	fi
-done
-echo "All virtual machines started!"
-# echo ""
+        echo "Starting VM: $VM"
+        ~/itops-scripts/physical-machines/vm-on.sh "$VM"
+        RC=$?
+        case $RC in
+                0)
+                        echo "Waiting $POST_START_DLY seconds for $VM services startup ..."
+                        sleep $POST_START_DLY
+                        ;;
+                1)
+                        echo "$VM already running, skipping delay"
+                        ;;
+                *)
+                        echo "Warning: $VM failed to start (exit $RC), continuing with next VM"
+                        ;;
+        esac
+        echo ""
 
+done
+echo "All virtual machines processed"
